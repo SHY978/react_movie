efe0a72cbe99a0314b1237cf24d43614ed1b48af
shy978 <e850120@naver.com>
2022-09-19 17:34:24 +0900

    220919

--- src/routes/members/Login.js
+++ src/routes/members/Login.js
@@ -2,8 +2,13 @@
 
 function Login(){
     return(      
-        <div>
-            <h1>hi</h1>
+        <div className="login_form">
+            <p>아이디</p>
+            <input type="text" placeholder="아이디를 입력 해 주세요"/><br/>
+            <p>비밀번호</p>
+            <input type="password" placeholder="비밀번호를 입력 해 주세요"/>
+            <br/>
+            <input type="button" value={'로그인'}/>
         </div>         
     );
 }
